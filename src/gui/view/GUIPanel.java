1a0bbdf7885e8e4ac1e28a4400412c84e222cdf4
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-11-13 12:19:20 -0700

    Build my framework for my JPanel.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -6,7 +6,14 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.SpringLayout;
 
-public class GUIPanel
+public class GUIPanel extends JPanel
 {
-
+	private GUIAppController appController;
+	public GUIPanel(GUIAppController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		
+	}
 }
