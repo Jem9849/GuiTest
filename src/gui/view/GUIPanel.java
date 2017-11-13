b27386a0740e8d51844042a7ce99b0e526e7395f
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-11-13 12:16:20 -0700

    A bunch of imports were made for a new class in the view package called GUIPanel.

---
+++ src/gui/view/GUIPanel.java
@@ -0,0 +1,12 @@
+package gui.view;
+
+import javax.swing.JPanel;
+import gui.controller.GUIAppController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+
+public class GUIPanel
+{
+
+}
